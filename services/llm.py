ccd986099128c73c2953e1e27e1d316c32210509
developerVincentAmbrosio <vince.ambrosio@gmail.com>
2026-09-10 19:53:05 -0400

    wrote basic llm object

---
+++ services/llm.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+model = ChatOpenAI(model="gpt-4", temperature=0)
